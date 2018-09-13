8534cdd3d47208bf84502107c65d510279c304ba
Odoo Aktiv <odoo@aktivsoftware.com>
2018-09-13 15:56:14 +0530

    [IMP] added summary in manifest file and change the position of banner and icon.

--- quick_validate_receipt/__manifest__.py
+++ quick_validate_receipt/__manifest__.py
@@ -4,6 +4,7 @@
     'category': 'Stock',
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
+    'summary': 'This module will validate multiple receipts with updated received quantity and source location.',
     'website': 'http://www.aktivsoftware.com',
     'author': 'Aktiv Software',
     'license': 'AGPL-3',
